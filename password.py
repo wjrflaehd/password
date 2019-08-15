973bf82305a167cc9dfe16497761a72c1150e3ef
Lyndon <lyndon.zhai@hotmail.com>
2019-08-16 00:38:09 +0900

    update version

--- password.py
+++ password.py
@@ -5,14 +5,12 @@
 # 如果正确，就印出 "登入成功！"
 # 如果不正确， 就印出 "密码错误！ 还有__次机会！"
 password = 'a123456'
-i = 3
-while True:
+i = 3 #剩余机会
+while i > 0:
 	pwd = input('请输入密码： ')
 	if pwd == password:
 		print('登入成功！')
-		break
+		break # 逃出回路
 	else:
 		i = i - 1
 		print('密码错误！ 还有', i, '次机会！')
-		if i == 0:
-			break
